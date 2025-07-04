8eb83cf3b6ea60af9cbc3096a405ca10a18fd73f
alpharabbit9 <alpharabbit74@gmail.com>
2025-07-05 00:42:20 +0600

    Project Completed

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.chblh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const teacherCollection = client.db('LeadingUniversityDB').collection('teachers')
+    const courseCollection = client.db('LeadingUniversityDB').collection('courses')
+
+    // Teacher Collection
+    app.get('/teachers/:department', async (req, res) => {
+
+      const department = req.params.department;
+      const query = { department: department };
+      const result = await teacherCollection.find(query).toArray();
+
+      res.send(result)
+    })
+
+    app.get('/teacherDetails/:id', async (req, res) => {
+
+      const id = req.params.id
+
+      const query = { _id: new ObjectId(id) };
+
+      const result = await teacherCollection.findOne(query);
+
+      res.send(result)
+    })
+
+
+    // Course Collection
+
+    app.get('/courses' , async(req,res) => {
+
+      // const department = req.params.department;
+
+      // const query = {department : department};
+
+      const result = await courseCollection.find().toArray();
+
+      res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+
+  res.send("Promise to Lead")
+})
+
+app.listen(port, () => {
+  console.log(`Reporting from port : ${port}`)
+})
